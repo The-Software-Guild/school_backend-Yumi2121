52192c42dc4f804f2f18ae8c1c3f2558d005afe4
Zumi Yu <88576446+Yumi2121@users.noreply.github.com>
2023-06-19 23:54:22 +1000

    Update CourseDaoImpl.java

--- java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/dao/CourseDaoImpl.java
+++ java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/dao/CourseDaoImpl.java
@@ -52,8 +52,8 @@ public class CourseDaoImpl implements CourseDao {
     @Override
     public void updateCourse(Course course) {
         //YOUR CODE STARTS HERE
-    	String sql = "UPDATE course SET courseCode=?, courseDesc=? WHERE cid=?";
-    	jdbcTemplate.update(sql, course.getCourseId(), course.getCourseName(), course.getCourseDesc());
+    	String sql = "UPDATE course SET courseCode=?, courseDesc=?, teacherId=? WHERE cid=?";
+    	jdbcTemplate.update(sql, course.getCourseId(), course.getCourseName(), course.getCourseDesc(), course.getTeacherId());
  
         //YOUR CODE ENDS HERE
     }
